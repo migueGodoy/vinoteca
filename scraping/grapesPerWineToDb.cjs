ef4952c42944acb7f42310cf179dabb9878ee69a
mgodoy <mgodoy@power-electronics.com>
2023-01-20 12:39:10 +0100

    submit grapes per wine to DB

---
+++ scraping/grapesPerWineToDb.cjs
@@ -0,0 +1,30 @@
+const mysql = require('mysql')
+const wines = require('../db/wines.json')
+
+const connection = mysql.createConnection({
+  host: 'sql11.freemysqlhosting.net',
+  user: 'sql11591990',
+  password: 'kblNu74VZd',
+  database: 'sql11591990'
+})
+
+connection.connect()
+
+const grapesPerWines = []
+wines.forEach(wine => {
+  const { id, grapes, ...restOfWine } = wine
+  grapes?.forEach(grape => {
+    const grapePerWine = {
+      grape: grape.id,
+      wine: wine.id
+    }
+
+    grapesPerWines.push(grapePerWine)
+  })
+})
+
+// grapesPerWines.forEach(grapePerWine => {
+//   connection.query('INSERT INTO grapesPerWine SET ?', grapePerWine, function (error, results, fields) {
+//     if (error) console.log(error)
+//   })
+// })
