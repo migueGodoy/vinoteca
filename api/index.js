99b46600087dc0ebb6acd1866f4fe2d8a63da752
mgodoy <mgodoy@power-electronics.com>
2023-01-24 11:30:11 +0100

    add endpoint for grapes

--- api/index.js
+++ api/index.js
@@ -9,11 +9,28 @@ app.get('/', ctx => {
       {
         url: '/wines',
         description: 'get all wines'
+      },
+      {
+        url: '/grapes',
+        description: 'get all grapes'
       }
     ]
   })
 })
 
 app.get('/wines', ctx => ctx.json(wines))
+
+app.get('/grapes', ctx => {
+  const allGrapes = []
+  wines.forEach(wine => {
+    const { grapes } = wine
+    if (grapes?.length) allGrapes.push(...grapes)
+  })
+
+  const grapesWithoutDuplicatesAndSorted = Array.from(new Set(allGrapes.map(grape => grape.id)))
+    .map(id => allGrapes.find(a => a.id === id)).sort((a, b) => a.id - b.id)
+
+  return ctx.json(grapesWithoutDuplicatesAndSorted)
+})
 
 export default app
