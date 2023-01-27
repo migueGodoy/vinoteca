2e652fd04a35b3ded864418f7165cd8570a9b6ae
Migue Godoy <mgodoysoro1988@gmail.com>
2023-01-27 22:20:08 +0100

    add some properties to wine

--- scraping/index.js
+++ scraping/index.js
@@ -3,9 +3,9 @@ import path from 'node:path'
 
 const getWines = async (pages) => {
   const wines = []
-  for (let i = 0; i < 68; i++) {
+  for (let i = 0; i < pages; i++) {
     console.log('Starting i: ' + i)
-    for (let j = 0; j < 10; j++) {
+    for (let j = 0; j <= 9; j++) {
       console.log({ j })
       const url = `https://www.vivino.com/api/explore/explore?country_code=ES&currency_code=EUR&min_rating=1&order_by=discount_percent&order=desc&price_range_max=50000&price_range_min=0&wine_type_ids%5B%5D=1&wine_type_ids%5B%5D=2&wine_type_ids%5B%5D=7&page=${i}${j}&language=es&per_page=50`
       const winesPerPage = await fetch(url)
@@ -25,6 +25,8 @@ const getWines = async (pages) => {
           country: wine?.vintage?.wine?.region?.country?.name,
           rating: wine?.vintage?.statistics?.ratings_average,
           ratingCount: wine?.vintage?.statistics?.ratings_count,
+          wineRating: wine?.vintage?.statistics?.wine_ratings_average,
+          wineRatingCount: wine?.vintage?.statistics?.wine_ratings_count,
           price: wine?.price?.amount,
           originalPrice: wine?.price?.discounted_from,
           currency: wine?.price?.currency?.code,
@@ -42,13 +44,16 @@ const getWines = async (pages) => {
             sweetness: wine?.vintage?.wine?.taste?.structure?.sweetness,
             tannin: wine?.vintage?.wine?.taste?.structure?.tannin,
             userStructureCount: wine?.vintage?.wine?.taste?.structure?.calculated_structure_count
-          }
+          },
+          pageFrom: `${i}${j}`
         }
       })
 
       wines.push(...winesWithOnlyInterestingProperties)
     }
   }
+
+  return wines
 }
 
 // const winesWithoutDuplicates = Array.from(new Set(wines.map(wine => wine.id)))
@@ -62,5 +67,5 @@ const writeWinesToFile = async (wines) => {
   await writeFile(filePath, JSON.stringify(wines, null, 2), 'utf-8')
 }
 
-const wines = await getWines(0) // 69
+const wines = await getWines(69) // 69
 writeWinesToFile(wines)
