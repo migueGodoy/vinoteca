f13e9e2ce43dfe69b52dc275dbc13f2a3214b28f
Migue Godoy <mgodoysoro1988@gmail.com>
2023-01-27 12:57:16 +0100

    separate into functions and get more data per wine

--- scraping/index.js
+++ scraping/index.js
@@ -1,54 +1,66 @@
 import { writeFile } from 'node:fs/promises'
 import path from 'node:path'
 
-const wines = []
+const getWines = async (pages) => {
+  const wines = []
+  for (let i = 0; i < 68; i++) {
+    console.log('Starting i: ' + i)
+    for (let j = 0; j < 10; j++) {
+      console.log({ j })
+      const url = `https://www.vivino.com/api/explore/explore?country_code=ES&currency_code=EUR&min_rating=1&order_by=discount_percent&order=desc&price_range_max=50000&price_range_min=0&wine_type_ids%5B%5D=1&wine_type_ids%5B%5D=2&wine_type_ids%5B%5D=7&page=${i}${j}&language=es&per_page=50`
+      const winesPerPage = await fetch(url)
+        .then(response => response.json())
+        .then(data => data.explore_vintage.matches)
 
-for (let i = 0; i < 68; i++) {
-  console.log('Starting i: ' + i)
-  for (let j = 0; j < 10; j++) {
-    console.log({ j })
-    const url = `https://www.vivino.com/api/explore/explore?country_code=ES&currency_code=EUR&grape_filter=varietal&min_rating=1&order_by=discount_percent&order=desc&price_range_max=50000&price_range_min=0&wine_type_ids%5B%5D=1&wine_type_ids%5B%5D=2&wine_type_ids%5B%5D=7&page=${i}${j}&language=es&per_page=50`
-    const winesPerPage = await fetch(url)
-      .then(response => response.json())
-      .then(data => data.explore_vintage.matches)
-
-    const winesWithOnlyInterestingProperties = winesPerPage.map(wine => {
-      return {
-        id: wine?.vintage?.wine?.id,
-        name: wine?.vintage?.wine?.name,
-        winery: wine?.vintage?.wine?.winery?.name,
-        year: wine?.vintage?.year,
-        region: wine?.vintage?.wine?.region?.name,
-        country: wine?.vintage?.wine?.region?.country?.name,
-        rating: wine?.vintage?.statistics?.ratings_average,
-        ratingCount: wine?.vintage?.statistics?.ratings_count,
-        price: wine?.price?.amount,
-        originalPrice: wine?.price?.discounted_from,
-        currency: wine?.price?.currency?.code,
-        volume: wine?.price?.bottle_type?.volume_ml,
-        grapes: wine?.vintage?.wine?.style?.grapes?.map(grape => (
-          {
-            id: grape?.id,
-            name: grape?.name
+      const winesWithOnlyInterestingProperties = winesPerPage.map(wine => {
+        return {
+          vintageId: wine?.vintage?.id,
+          wineId: wine?.vintage?.wine?.id,
+          vintageName: wine?.vintage?.name,
+          wineName: wine?.vintage?.wine?.name,
+          typeId: wine?.vintage?.wine?.type_id,
+          winery: wine?.vintage?.wine?.winery?.name,
+          year: wine?.vintage?.year,
+          region: wine?.vintage?.wine?.region?.name,
+          country: wine?.vintage?.wine?.region?.country?.name,
+          rating: wine?.vintage?.statistics?.ratings_average,
+          ratingCount: wine?.vintage?.statistics?.ratings_count,
+          price: wine?.price?.amount,
+          originalPrice: wine?.price?.discounted_from,
+          currency: wine?.price?.currency?.code,
+          volume: wine?.price?.bottle_type?.volume_ml,
+          grapes: wine?.vintage?.wine?.style?.grapes?.map(grape => (
+            {
+              id: grape?.id,
+              name: grape?.name
+            }
+          )),
+          taste: {
+            acidity: wine?.vintage?.wine?.taste?.structure?.acidity,
+            fizziness: wine?.vintage?.wine?.taste?.structure?.fizziness,
+            intensity: wine?.vintage?.wine?.taste?.structure?.intensity,
+            sweetness: wine?.vintage?.wine?.taste?.structure?.sweetness,
+            tannin: wine?.vintage?.wine?.taste?.structure?.tannin,
+            userStructureCount: wine?.vintage?.wine?.taste?.structure?.calculated_structure_count
           }
-        )),
-        taste: {
-          acidity: wine?.vintage?.wine?.taste?.structure?.acidity,
-          fizziness: wine?.vintage?.wine?.taste?.structure?.fizziness,
-          intensity: wine?.vintage?.wine?.taste?.structure?.intensity,
-          sweetness: wine?.vintage?.wine?.taste?.structure?.sweetness,
-          tannin: wine?.vintage?.wine?.taste?.structure?.tannin,
-          userStructureCount: wine?.vintage?.wine?.taste?.structure?.calculated_structure_count
         }
-      }
-    })
+      })
 
-    wines.push(...winesWithOnlyInterestingProperties)
+      wines.push(...winesWithOnlyInterestingProperties)
+    }
   }
 }
 
-const winesWithoutDuplicates = Array.from(new Set(wines.map(wine => wine.id)))
-  .map(id => wines.find(a => a.id === id))
+// const winesWithoutDuplicates = Array.from(new Set(wines.map(wine => wine.id)))
+//   .map(id => wines.find(a => a.id === id))
 
-const filePath = path.join(process.cwd(), 'db', 'wines.json')
-await writeFile(filePath, JSON.stringify(winesWithoutDuplicates, null, 2), 'utf-8')
+// const filePath = path.join(process.cwd(), 'db', 'wines.json')
+// await writeFile(filePath, JSON.stringify(wines, null, 2), 'utf-8')
+
+const writeWinesToFile = async (wines) => {
+  const filePath = path.join(process.cwd(), 'db', 'wines.json')
+  await writeFile(filePath, JSON.stringify(wines, null, 2), 'utf-8')
+}
+
+const wines = await getWines(0) // 69
+writeWinesToFile(wines)
